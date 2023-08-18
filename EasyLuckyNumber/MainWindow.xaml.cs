61c50b3e90fd9ef869c2060f207dcf46fc43e055
phongphong.28 <119289183+P-ro-VL@users.noreply.github.com>
2023-08-19 01:38:17 +0700

    Init Commit

---
+++ EasyLuckyNumber/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace EasyLuckyNumber
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public static List<int> data = new List<int>();
+        public static MainWindow Instance;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Instance = this;
+
+            KeyDown += (s, e) =>
+            {
+                if(e.Key == Key.Escape)
+                {
+                    Settings settingWindow = new Settings();
+                    settingWindow.Show();
+                }else if(e.Key == Key.Space)
+                {
+                    if(data.Count == 0)
+                    {
+                        MessageBox.Show("Vui lòng thiết lập tệp dữ liệu trước khi bắt đầu quay bằng cách nhấn nút ESC.");
+                        return;
+                    }
+
+                    spinning();
+                }else if(e.Key == Key.R)
+                {
+                    displayNumber.Text = "000";
+                }
+            };
+        }
+
+        private void spinning()
+        {
+            Random rand = new Random();
+            int index = rand.Next(data.Count);
+            int luckyNumber = data[index];
+
+            int t = 0;
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval += TimeSpan.FromMilliseconds(100);
+            timer.Tick += (s, e) =>
+            {
+                if(t >= 3*1000)
+                {
+                    displayNumber.Text = luckyNumber.ToString("D3");
+                    data.Remove(luckyNumber);
+
+                    timer.Stop();
+                }else
+                {
+                    displayNumber.Text = rand.Next(999).ToString("D3");
+                    t+=100;
+                }
+            };
+            timer.Start();
+        }
+
+    }
+}
---
+++ EasyLuckyNumber/Settings.xaml.cs
@@ -0,0 +1,133 @@
+﻿using IronXL;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using FontFamily = System.Drawing.FontFamily;
+
+namespace EasyLuckyNumber
+{
+    /// <summary>
+    /// Interaction logic for Settings.xaml
+    /// </summary>
+    public partial class Settings : Window
+    {
+        public static string dataPath = "";
+        public static string backgroundURLPath = "";
+        public static string fontFamily = "";
+        public static string fontSize = "200";
+
+        public Dictionary<string, FontFamily> loadedFonts = new Dictionary<string, FontFamily>();
+
+        public Settings()
+        {
+            InitializeComponent();
+
+            dataFilePath.Text = dataPath;
+            backgroundPath.Text = backgroundURLPath;
+            fontSizeInput.Text = fontSize + "";
+
+            InstalledFontCollection installedFontCollection = new InstalledFontCollection();
+            
+            foreach (var font in installedFontCollection.Families)
+            {
+                fontFamilyInput.Items.Add(font.Name);
+                loadedFonts.Add(font.Name, font);
+            }
+
+            fontFamilyInput.SelectedItem = fontFamily;
+
+            dataFileButton.Click += (s, e) =>
+            {
+                selectDataFile();
+            };
+
+            backgroundFile.Click += (s, e) =>
+            {
+                selectBackgroundFile();
+            };
+
+            finishButton.Click += (s, e) =>
+            {
+                fontSize = fontSizeInput.Text;
+
+                if(fontFamilyInput.SelectedItem != null)
+                    fontFamily = fontFamilyInput.Text;
+
+                MainWindow.Instance.displayNumber.FontSize = int.Parse(fontSize);
+                System.Windows.Media.FontFamily mfont = new System.Windows.Media.FontFamily(fontFamily);
+                MainWindow.Instance.displayNumber.FontFamily = mfont;
+
+                Hide();
+            };
+
+            exitButton.MouseDown += (s, e) =>
+            {
+                Hide();
+            };
+
+        }
+
+        private void selectBackgroundFile()
+        {
+            OpenFileDialog fileSelector = new OpenFileDialog();
+            fileSelector.Filter = "Image Files (*.*)|*.*"; ;
+            if (fileSelector.ShowDialog() == true)
+            {
+                string filePath = fileSelector.FileName;
+
+                try
+                {
+                    BitmapImage image = new BitmapImage(new Uri(filePath));
+                    MainWindow.Instance.background.Source = image;
+
+                    backgroundPath.Text = filePath;
+                    MessageBox.Show("Thay đổi hình nền thành công!");
+
+                    backgroundURLPath = filePath;
+
+                }catch (Exception ex)
+                {
+                    MessageBox.Show("Unable to load background image! \n" + ex.ToString());
+                }
+            }
+        }
+
+        private void selectDataFile()
+        {
+            OpenFileDialog fileSelector = new OpenFileDialog();
+            fileSelector.Filter = "Excel Files (*.xlsx)|*.xlsx|All files (*.*)|*.*"; ;
+            if(fileSelector.ShowDialog() == true)
+            {
+                string filePath = fileSelector.FileName;
+
+                try
+                {
+                    WorkBook workBook = WorkBook.Load(filePath);
+                    WorkSheet workSheet = workBook.WorkSheets[0];
+                    RangeColumn dataColumn = workSheet.GetColumn("A");
+                    List<int> data = dataColumn.Select(x => x.Int32Value).ToList();
+
+                    MainWindow.data = data;
+
+                    MessageBox.Show("Tải dữ liệu thành công!\nĐã tải: " + data.Count + " bản ghi.");
+                
+                    dataFilePath.Text = filePath;
+                    dataPath = filePath;
+                } catch (Exception ex)
+                {
+                    MessageBox.Show("An unexpected error has occured while trying to load data from "
+                        + filePath + "!\n" + ex.ToString());
+                }
+            }
+        }
+
+    }
+}
